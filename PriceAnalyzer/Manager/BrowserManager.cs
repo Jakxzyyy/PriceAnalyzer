e0030f0a058fb26daba24a3c8b5abb1315649e99
Max <xxtamp@yandex.ru>
2025-02-05 19:00:32 +0300

    first commit

---
+++ PriceAnalyzer/Manager/BrowserManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using PuppeteerSharp;
+
+namespace PriceAnalyzer.Manager
+{
+    public class BrowserManager
+    {
+        public Browser browser;
+        public Page page;
+        LaunchOptions launchOptions;
+        private List<string> sites;
+
+        public BrowserManager()
+        {
+            string browserPath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+            launchOptions.ExecutablePath = browserPath;
+            string sitesTxtPath = @"\PriceAnalyzer\Resources\Sites.txt";
+            sites = File.ReadAllLines(sitesTxtPath).ToList();
+        }
+
+        public async Task<string> LoadHtmlPage(string url)
+        {
+            if (browser == null)
+            {
+                await InitBrowser();
+                page = await GetPage();
+            }
+            var response = await page.GoToAsync(url);
+            var jsonString = await response.TextAsync();
+
+            await browser.DisposeAsync();
+
+            return jsonString;
+        }
+        public async Task InitBrowser()
+        {
+            browser = (Browser)await Puppeteer.LaunchAsync(launchOptions);
+        }
+        public async Task<Page> GetPage()
+        {
+            return (Page)await browser.NewPageAsync();
+        }
+        public async Task<List<string>> GetUrls()
+        {
+            List<string> urls = null; //чота
+            return urls;
+        }
+    }
+}
---
+++ PriceAnalyzer/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceAnalyzer.Models
+{
+    internal class Product
+    {
+        public float Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        
+        public Product(float price, string name, string description, string link)
+        {
+            Price = price;
+            Name = name;
+            Description = description;
+            Link = link;
+        }
+    }
+}
